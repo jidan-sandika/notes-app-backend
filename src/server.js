7172a34ea84ad2bbb475bbd735e49893ca0beac5
Jidan <zidansandika06@gmail.com>
2022-04-29 15:05:13 +0700

    Initial Commit

---
+++ src/server.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable padded-blocks */
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => { 
+    
+  const server = Hapi.server({
+
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+
+  });
+
+  server.route(routes);
+
+  await server.start();
+
+  console.log(`Server Berjalan Pada ${server.info.uri}`);
+
+};
+
+init();
